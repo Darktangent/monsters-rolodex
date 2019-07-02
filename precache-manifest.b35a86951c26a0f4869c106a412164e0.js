80655e31809e08e7b8c01d8b25b940ab558ab01a
Ron Ganguly <c1ph3r9@gmail.com>
2019-07-02 11:19:49 -0500

    Updates

---
+++ precache-manifest.b35a86951c26a0f4869c106a412164e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bef3daef162485732340baa06ccc63c",
+    "url": "darktangent.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5a3878f74fa3112917d8",
+    "url": "darktangent.github.io/monsters-rolodex/static/css/main.2583ffc2.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "darktangent.github.io/monsters-rolodex/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "5a3878f74fa3112917d8",
+    "url": "darktangent.github.io/monsters-rolodex/static/js/main.68b5f52a.chunk.js"
+  },
+  {
+    "revision": "760f888a6d02c5b248ba",
+    "url": "darktangent.github.io/monsters-rolodex/static/js/runtime~main.11802763.js"
+  }
+]);
